566aa1837f3ac00922551f8093996c7e4fc62a37
DimDey <dimdey.deyna@gmail.com>
2018-10-23 16:06:00 +0300

    Тестовая версия логин-панели

--- Client/global_variables.lua
+++ Client/global_variables.lua
@@ -5,6 +5,8 @@
 screenW, screenH = guiGetScreenSize() -- get player`s screen size
 DGS = exports.dgs -- exports from dgs
 -- регистрация и авторизация
+editWidth = 400
+editHeight = 35
 login = DGS:dgsCreateEdit( (screenW - editWidth) / 2, (screenH - editHeight ) / 2 - 35, editWidth, editHeight, "Логин", false, false, tocolor(0,0,0,255),1,1,false,tocolor(109, 118, 120) )
 pass = DGS:dgsCreateEdit( (screenW - editWidth) / 2, (screenH - editHeight ) / 2 + 35, editWidth, editHeight, "Пароль", false, false, tocolor(0,0,0,255),1,1,false,tocolor(109, 118, 120) )
 submit = DGS:dgsCreateButton( (screenW - editWidth) / 2, (screenH - editHeight ) / 2 + 75, editWidth, editHeight, "Авторизация", false )
