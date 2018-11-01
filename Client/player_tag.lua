c107f499a18fa762457bb1ed739f42077b9af7dd
DimDey <dimdey.deyna@gmail.com>
2018-11-01 19:54:26 +0300

    Тест.

--- Client/player_tag.lua
+++ Client/player_tag.lua
@@ -9,7 +9,7 @@ function dxDrawTextOnElement(TheElement,text,height,distance,R,G,B,alpha,size,fo
 		if(sx) and (sy) then
 			local distanceBetweenPoints = getDistanceBetweenPoints3D(x, y, z, x2, y2, z2)
 			if(distanceBetweenPoints < distance) then
-				dxDrawText(text, sx, sy, sx-1, sy-1, tocolor(R or 255, G or 255, B or 255, alpha or 255), (size or 1.2)-(distanceBetweenPoints / distance), font or "default-bold", "center", "center")
+				dxDrawText(text, sx, sy, sx-1, sy-1, tocolor(R or 255, G or 255, B or 255, alpha or 255), (size or 1.2)-(distanceBetweenPoints * distance), font or "default-bold", "center", "center")
 			end
 		end
 	end
@@ -19,7 +19,7 @@ addEventHandler("onClientRender", getRootElement(),
 function ()
 	for k,v in ipairs(getElementsByType("player")) do
 		if isElementStreamedIn( v ) then
-			if isLogged(v) then
+			if isLogged(v) and getElementData(v,"id")  then
 				if v ~= lp then
 					dxDrawTextOnElement(v,getElementData( v,"nick").." [ "..getElementData(v,"id").." ]",1,20,255,255,255)
 				end
