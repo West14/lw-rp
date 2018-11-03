e2e4209d36d80e2486cbfdb35deb88cfab4b8ff2
DimDey <dimdey.deyna@gmail.com>
2018-11-03 16:43:19 +0300

    Переписал функцию оутпута в чат, осталось переписать ивенты

--- Client/chat_general.lua
+++ Client/chat_general.lua
@@ -64,41 +64,23 @@ function chat_onDGSBlur()
 	--outputDebugString("Editbox blured")
 end
 
-function outputChatMessage( msg ) -- написать сообщение
-	table.insert(chat_messages,"[ "..hours..":"..minutes..":"..seconds.." ] "..msg) -- исёртим сообщение в таблицу
+function outputChatMessage( msg, hexColor ) -- написать сообщение
+	table.insert(chat_messages,hexColor or "#FFFFFF".."[ "..hours..":"..minutes..":"..seconds.." ] "..msg) -- исёртим сообщение в таблицу
 	TextFuel() -- отрисовываем чат
-	outputConsole("Chat: "..msg)
 end
 addEvent("outputChatMessage",true) -- добавляем ивент для триггера с серверной части
 addEventHandler("outputChatMessage",root,outputChatMessage) -- добавляем хандлер ивента
-
-function outputError( msg )
-	table.insert(chat_messages,"#990000[ "..hours..":"..minutes..":"..seconds.." ] "..msg) -- исёртим сообщение в таблицу
-	TextFuel() -- отрисовываем чат
-	outputConsole("Chat: "..msg)
-end
-addEvent("outputError",true) -- добавляем ивент для триггера с серверной части
-addEventHandler("outputError",root,outputError) -- добавляем хандлер ивента
-
-function outputSuccess( msg )
-	table.insert(chat_messages,"#009900[ "..hours..":"..minutes..":"..seconds.." ] "..msg) -- исёртим сообщение в таблицу
-	TextFuel() -- отрисовываем чат
-	outputConsole("Chat: "..msg)
-end
-addEvent("outputSuccess",true) -- добавляем ивент для триггера с серверной части
-addEventHandler("outputSuccess",root,outputSuccess) -- добавляем хандлер ивента
 
 function outputAdminChatMessage( msgNick, msg )
 	local message = ""
 	for i, theMsg in pairs(msg) do
 		message = message.." "..theMsg
 	end
-	table.insert(chat_messages,"#ADFF2F[ "..hours..":"..minutes..":"..seconds.." ] [ Admin Chat ]: "..msgNick..message) -- исёртим сообщение в таблицу
-	TextFuel() -- отрисовываем чат
-	outputConsole("Achat: "..message)
+	outputChatMessage("[ Admin Chat ]: "..msgNick..message,"#ADFF2F")
 end
 addEvent( "outputAdminChatMessage",true)
 addEventHandler( "outputAdminChatMessage", root, outputAdminChatMessage)	
+
 
 function clearChatBox( )
 	DGS:dgsSetText ( chatBox, "" ) -- обнуление текста
