597f6b6f84a53ee5df23bafdc57b157a476e451b
DimDey <dimdey.deyna@gmail.com>
2018-11-04 01:11:47 +0300

    Вроде-бы пофиксил авторизацию, подписал афк систему, но она всё ещё не работает.
    Signed-off-by: DimDey <dimdey.deyna@gmail.com>

--- Client/afk_system.lua
+++ Client/afk_system.lua
@@ -1,20 +1,23 @@
---[[afk_time = 0
+afk_time = 0
 openedesc = false
 function startAFKtimer( )
-	afk_timer = setTimer( setTimerValue, 1000, -1 )
+	if afk_timer then
+		stopAFKtimer( )
+	else
+		openedesc = true
+		afk_timer = setTimer( setTimerValue, 1000, 0 )
+	end
+	
 end
-addEventHandler( "onClientMinimize", getRootElement(), startAFKtimer )
 
 function onKey( button, state )
 	if isLogged(lp) then
 		if state then
 			if button == "escape" then
 				if openedesc then
-					openedesc = false
 					stopAFKtimer( )
 				else
-					openedesc = true
-					afk_timer = setTimer( setTimerValue, 1000, -1 )
+					startAFKtimer()
 				end
 			end
 		end
@@ -23,10 +26,12 @@ end
 addEventHandler( "onClientKey", getRootElement(), onKey )
 
 function setTimerValue( )
-	afk_time = afk_time + 1
+		afk_time = afk_time + 1
+		outputChatMessage("Вы были в афк "..afk_time.." секунд!","#00FF00")
 end
 
 function stopAFKtimer( )
-	resetTimer( afk_timer )
+	killTimer( afk_timer )
+	afk_time = 0
+	openedesc = false
 end
-addEventHandler( "onClientRestore", getRootElement(), stopAFKtimer )]]
